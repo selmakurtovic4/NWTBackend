70fa69a304963adca81db40ebe55c2a64348ca9c
apandz <amina.pandzic26@gmail.com>
2024-04-03 10:02:43 +0200

    Fixed error

--- medical-record/src/main/java/com/hoperise/medicalrecord/controller/MedicalInformationController.java
+++ medical-record/src/main/java/com/hoperise/medicalrecord/controller/MedicalInformationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/medical-report")
+@RequestMapping("/medical-information")
 public class MedicalInformationController {
 
     @Autowired
