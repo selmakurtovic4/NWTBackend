984aa2881d6f80478bf0ae20e4d8567d7ae2ca54
apandz <amina.pandzic26@gmail.com>
2024-04-05 10:54:07 +0200

    DoctorReferral model created and implemented

---
+++ medical-record/src/main/java/com/hoperise/medicalrecord/model/DoctorReferral.java
@@ -0,0 +1,165 @@
+package com.hoperise.medicalrecord.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "doctor-referral")
+public class DoctorReferral {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "reason")
+    @NotNull(message = "Reason can't be null!")
+    private String reason;
+    @Column(name = "priority")
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Priority priority;
+    @Column(name = "date")
+    @NotNull
+    private LocalDate date;
+    @Column(name = "referring_doctor_id")
+    @NotNull
+    private Long referringDoctorId;
+    @Column(name = "patient_id")
+    @NotNull
+    private Long patientId;
+    @Column(name = "referred_doctor_id")
+    @NotNull
+    private Long referredDoctorId;
+    @Column(name = "created")
+    @NotNull(message = "Creation date must be specified!")
+    private LocalDateTime created;
+    @Column(name = "created_by")
+    @NotNull(message = "User that created the appointment must be specified!")
+    private String createdBy;
+    @Column(name = "modified")
+    private LocalDateTime modified;
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    public DoctorReferral() {
+    }
+
+    public DoctorReferral(String reason, Priority priority, LocalDate date, Long referringDoctorId, Long patientId, Long referredDoctorId, LocalDateTime created, String createdBy, LocalDateTime modified, String modifiedBy) {
+        this.reason = reason;
+        this.priority = priority;
+        this.date = date;
+        this.referringDoctorId = referringDoctorId;
+        this.patientId = patientId;
+        this.referredDoctorId = referredDoctorId;
+        this.created = created;
+        this.createdBy = createdBy;
+        this.modified = modified;
+        this.modifiedBy = modifiedBy;
+    }
+
+    public DoctorReferral(String reason, Priority priority, LocalDate date, Long referringDoctorId, Long patientId, Long referredDoctorId, LocalDateTime created, String createdBy) {
+        this.reason = reason;
+        this.priority = priority;
+        this.date = date;
+        this.referringDoctorId = referringDoctorId;
+        this.patientId = patientId;
+        this.referredDoctorId = referredDoctorId;
+        this.created = created;
+        this.createdBy = createdBy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Long getReferringDoctorId() {
+        return referringDoctorId;
+    }
+
+    public void setReferringDoctorId(Long referringDoctorId) {
+        this.referringDoctorId = referringDoctorId;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Long getReferredDoctorId() {
+        return referredDoctorId;
+    }
+
+    public void setReferredDoctorId(Long referredDoctorId) {
+        this.referredDoctorId = referredDoctorId;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    @Override
+    public String toString() {
+        return "DoctorReferral{" + "id=" + id + ", reason='" + reason + '\'' + ", priority=" + priority + ", date=" + date + ", referringDoctorId=" + referringDoctorId + ", patientId=" + patientId + ", referredDoctorId=" + referredDoctorId + ", created=" + created + ", createdBy='" + createdBy + '\'' + ", modified=" + modified + ", modifiedBy='" + modifiedBy + '\'' + '}';
+    }
+}
