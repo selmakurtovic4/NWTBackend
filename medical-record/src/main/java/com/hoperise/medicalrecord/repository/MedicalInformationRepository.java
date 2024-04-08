5ff492e3a7f17f5d6be2eda71c43b3d3bc9d006a
apandz <amina.pandzic26@gmail.com>
2024-04-08 15:02:50 +0200

    Added method

--- medical-record/src/main/java/com/hoperise/medicalrecord/repository/MedicalInformationRepository.java
+++ medical-record/src/main/java/com/hoperise/medicalrecord/repository/MedicalInformationRepository.java
@@ -4,4 +4,5 @@ import com.hoperise.medicalrecord.model.MedicalInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicalInformationRepository extends JpaRepository<MedicalInformation, Long> {
+    boolean existsByPatientId(Long patientId);
 }
