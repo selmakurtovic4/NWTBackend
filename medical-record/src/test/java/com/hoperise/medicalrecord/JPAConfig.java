57fa9e9b0fe0e1a75acdd58b3a02fca46589e035
apandz <amina.pandzic26@gmail.com>
2024-04-08 13:56:58 +0200

    Configuration and files for tests created

---
+++ medical-record/src/test/java/com/hoperise/medicalrecord/JPAConfig.java
@@ -0,0 +1,27 @@
+package com.hoperise.medicalrecord;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.hoperise.medicalrecord")
+@EnableTransactionManagement
+public class JPAConfig {
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("ap2");
+        dataSource.setPassword("ap2");
+
+        return dataSource;
+    }
+}
---
+++ medical-record/src/test/java/com/hoperise/medicalrecord/MedicalReportControllerTest.java
@@ -0,0 +1,4 @@
+package com.hoperise.medicalrecord;
+
+public class MedicalReportControllerTest {
+}
