66cce14aa1a1b76a68194c3cf020ec1cf4612295
Adna <adna.mehanovic1@gmail.com>
2024-05-06 15:38:38 +0200

    Add gateway

---
+++ hoperise-gateway/src/main/java/com/example/hoperisegateway/HoperiseGatewayApplication.java
@@ -0,0 +1,29 @@
+package com.example.hoperisegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HoperiseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoperiseGatewayApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RouteLocator test(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("appointmentService", p -> p
+						.path("/appointment/**")
+						.uri("lb://appointment"))
+				.build();
+	}
+}
---
+++ hoperise-gateway/src/test/java/com/example/hoperisegateway/HoperiseGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoperisegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoperiseGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
