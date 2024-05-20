de1352439853442704f2e489d40e962da8e9fc02
apandzic2 <apandzic2@etf.unsa.ba>
2024-05-20 16:49:58 +0200

    Added new method

--- medical-record/src/main/java/com/hoperise/medicalrecord/repository/MedicalReportRepository.java
+++ medical-record/src/main/java/com/hoperise/medicalrecord/repository/MedicalReportRepository.java
@@ -2,8 +2,14 @@ package com.hoperise.medicalrecord.repository;
 
 import com.hoperise.medicalrecord.model.MedicalReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface MedicalReportRepository extends JpaRepository<MedicalReport, Long> {
+    @Query("SELECT CASE WHEN COUNT(mr) > 0 THEN true ELSE false END FROM MedicalReport mr WHERE mr.date = :date AND mr.patientId = :patientId")
+    boolean existsByDateAndPatientId(@Param("date") LocalDate date, @Param("patientId") Long patientId);
 }
